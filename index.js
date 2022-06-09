b89ea0191344b2aa19f41fccd57f303c783345da
AninHuang <lifeanne19904@gmail.com>
2022-06-10 00:42:28 +0800

    Implement debouncing

---
+++ index.js
@@ -0,0 +1,10 @@
+function debounce(callback, delay) {
+	let timerId;
+
+	return (...args) => {
+      if (timerId) clearTimeout(timerId)
+      timerId = setTimeout(() => {
+        callback(...args)
+      }, delay)
+    }
+}
